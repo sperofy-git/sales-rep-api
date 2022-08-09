fc024886a440cbbbc8beca340c338cb65f39f6af
kvsk <contact@sperofy.com>
2022-08-09 22:09:41 +0530

    backend updated with basic auth

---
+++ src/main/java/com/ssl/salesrepapi/security/SpringSecurityConfigurationBasicAuth.java
@@ -0,0 +1,44 @@
+package com.ssl.salesrepapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth{
+	
+
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+        	.csrf().disable()
+        	.antMatcher("/**")
+            .authorizeHttpRequests((authz) -> authz
+            	.antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+
+//	@Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//        	.csrf().disable()
+//        	.authorizeRequests()
+//        	.antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//            .httpBasic();
+//    }
+
+	
+	
+}
