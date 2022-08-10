a6078de6b7b11dccb67e50c172c45c05e80deded
kvsk <contact@sperofy.com>
2022-08-10 10:21:27 +0530

    backend updated with basic auth

---
+++ src/main/java/com/ssl/salesrepapi/security/BasicAuthController.java
@@ -0,0 +1,24 @@
+package com.ssl.salesrepapi.security;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssl.salesrepapi.beans.MyBean;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class BasicAuthController {
+	
+	
+	@GetMapping( path = "/basicauth")
+	public BasicAuthBean getBean() {
+		
+		return new BasicAuthBean("You are authenticated");
+	}
+	
+
+}
